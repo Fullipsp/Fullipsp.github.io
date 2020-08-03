138c9ca8a0e28475f6564b01d63f71057c4c9697
Fullipsp <69165390+Fullipsp@users.noreply.github.com>
2020-08-03 21:12:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -68,7 +68,20 @@ addPin('star.jpg', 'Star Bunny Witch', "Pre-ordered")
 
 
 
-
+$("html").easeScroll({
+    frameRate: 60,
+    animationTime: 1000,
+    stepSize: 120,
+    pulseAlgorithm: 1,
+    pulseScale: 8,
+    pulseNormalize: 1,
+    accelerationDelta: 20,
+    accelerationMax: 1,
+    keyboardSupport: true,
+    arrowScroll: 50,
+    touchpadSupport: true,
+    fixedBackground: true
+  });
 
 
 
