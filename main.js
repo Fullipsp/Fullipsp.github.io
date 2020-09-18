6895a353bdf9a160c04ed7c1af2ec84c85904bc6
Fullipsp <69165390+Fullipsp@users.noreply.github.com>
2020-09-18 19:13:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ addPin('zelda.jpg', 'Zelda (Nintendolls V2)', "Waiting")
 
 addCategory('', "")
 addInlinePin('mybuns.png', 'Two bunnies (Luna & Bianca)', "Owned", "Lilycli (UpbeatPals)", "https://lilycli.com")
-addInlinePin('BirthdayBun.png', 'Sprinkle Birthday Bunny', "Owned", "Sharodactylart", "https://sharodactylart.com")
+addInlinePin('BirthdayBun.jpg', 'Sprinkle Birthday Bunny', "Owned", "Sharodactylart", "https://sharodactylart.com")
 addInlinePin('weishi.jpg', 'Lilys Hanami (September 2020 Pin Club)', "Pre-ordered", "Weishi", "https://linktr.ee/weishi_art")
 
 addCategory('Xhilyn', "https://xhilyn.carrd.co")
