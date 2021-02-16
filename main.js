b898549cb5500daaead439d9dc0476e93468cfb1
Fullipsp <69165390+Fullipsp@users.noreply.github.com>
2021-02-16 23:00:13 +0000

    Update main.js

--- main.js
+++ main.js
@@ -93,6 +93,11 @@ addPin('witch usagi.jpg', 'Witch Usagi <br>(Fashion Fandom December 2020 Pin Clu
 addPin('snowbunny.jpg', 'Holly the Snow Bunny Mage <br>(Original December 2020 Pin Club)', "Shipped")
 addPin('BabyBunMage.jpg', 'Snow Mages Bunny Friend <br>(Original December 2020 Pin Club)', "Shipped")
 
+addCategory("Polymernai", "https://www.polymernai.com")
+
+addPin('VioletEvergarden.jpg', 'Violet x Gilbert Pin <br>(Nostalgia Couples KS)', "Pre-Ordered")
+addPin('Zelink.jpg', 'Zelda x Link Pin (Tartaglia) <br>(Nostalgia Couples KS)', "Pre-Ordered")
+addPin('Ladynoir.jpg', 'Ladybug x Cat Noir Pin <br>(Nostalgia Couples KS)', "Pre-Ordered")
 
 addCategory('', "")
 addInlinePin('MyBuns.jpg', 'Two bunnies <br>(Luna & Bianca)', "Owned", "Lilycli (UpbeatPals)", "https://lilycli.com")
