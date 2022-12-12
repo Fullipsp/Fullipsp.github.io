b137cd02ca2ff24fd1a0f2befe9b79d2851389c7
Supertiger <saawalkhn@googlemail.com>
2022-12-12 20:31:28 +0000

    updated

--- js/pins.js
+++ js/pins.js
@@ -1,14 +1,21 @@
 // cSpell:disable
 let categoryIndex = 0;
 
+
 addCategory("Mystic Messenger Pins", "http://msg.cheritz.com/")
     .addPin('gebby.jpg', 'GE Saeran Chibi <br>by Alicehwyy', "Owned")
     .addPin('raybby.jpg', 'Ray Chibi <br>by Alicehwyy', "Owned")
     .addPin('suitbby.jpg', 'Suit Saeran Chibi <br>by Alicehwyy', "Owned")
     .addPin('unknownbby.jpg', 'Unknown Chibi <br>by Alicehwyy', "Owned")
-    .addPin('sebby.jpg', 'SE Saeran Chibi by <br>Alicehwyy', "Owned")
+    .addPin('sebby.jpg', 'SE Saeran Chibi <br>by Alicehwyy', "Owned")
     .addPin('saeyoung.jpg', 'Saeyoung (707) <br>by Theartthatran', "Owned")
     .addPin('saeran.jpg', 'Saeran (Ray) <br>by Theartthatran', "Owned")
+
+    .addPin('napray.jpg', 'Ray Chibi <br>by Napporin', "Owned")
+    .addPin('napsuit.jpg', 'Suit Saeran Chibi <br>by Napporin', "Owned")
+    .addPin('napunknown.jpg', 'Unknown Chibi <br>by Napporin', "Owned")
+    .addPin('napsaeran.jpg', 'Wedding Saeran Chibi <br>by Napporin', "Owned")
+    .addPin('napsaeran.jpg', 'Wedding Saeran Ring <br>by Napporin', "Owned")
 
 addCategory("Genshin Impact Pins", "https://genshin.hoyoverse.com")
     .addPin('ChildeTraditional.jpg', 'Childe <br>by BirbsNest', "Owned")
@@ -18,12 +25,13 @@ addCategory("Genshin Impact Pins", "https://genshin.hoyoverse.com")
     .addPin('fiveitto.jpg', 'Itto <br>by Fiveonthe', "Shipped")
     .addPin('fivethoma.jpg', 'Thoma <br>by Fiveonthe', "Shipped")
 
-    .addPin('lengorou.jpg', 'Gorou by <br>Lenacchi', "Owned")
+
+    .addPin('lengorou.jpg', 'Gorou <br>by Lenacchi', "Owned")
     .addPin('lenzhongli.jpg', 'Zhongli <br>by Lenacchi', "Owned")
     .addPin('lenayato.jpg', 'Ayato <br>by Lenacchi', "Shipped")
-    .addPin('lenxiao.jpg', 'Xiao by <br>Lenacchi', "Shipped")
+    .addPin('lenxiao.jpg', 'Xiao <br>by Lenacchi', "Shipped")
     .addPin('lenkazuha.jpg', 'Kazuha <br>by Lenacchi', "Shipped")
-    .addPin('eggkazuha.jpg', 'Kazuha by angryeggmerch', "Waiting")
+    .addPin('eggkazuha.jpg', 'Kazuha <br>by angryeggmerch', "Shipped")
 
     .addPin('scara.jpg', 'Scaramouche <br>by Sugar Cubed Studios', "Owned")
     .addPin('lumine.jpg', 'Lumine <br>by Sugar Cubed Studios', "Owned")
@@ -40,20 +48,22 @@ addCategory("Genshin Impact Pins", "https://genshin.hoyoverse.com")
 
     .addPin('anemo.jpg', 'Anemoculus <br>by Theartthatran', "Owned")
 
-
-    .addPin('chilumi.jpg', 'Chilumi Hollichoo', "Owned")
-    .addPin('promise.jpg', 'Snezhnayan Promise by Hollichoo', "Owned")
+    .addPin('chilumi.jpg', 'Chilumi <br>by Hollichoo', "Owned")
+    .addPin('promise.jpg', 'Snezhnayan Promise <br>by Hollichoo', "Owned")
 
 addCategory("MDZS & Wangxian Pins",)
     .addPin('bluhdonkey.jpg', 'Wangxian Donkey <br>by Bluhtack', "Owned")
     .addPin('bluhroof.jpg', 'Wangxian on Roof <br>by Bluhtack', "Owned")
     .addPin('weiying.jpg', 'Valentine Wei Ying <br>by Billacookie', "Owned")
+    .addPin('MyBuns.jpg', 'Wangxian Bunnies <br>by Lilycli', "Owned")
 
 addCategory("Kirby Pins by ArcadeHealer", "https://arcadehealer.carrd.co")
     .addPin('OGKirby.jpg', 'Star Wand Kirby ⭐️', "Owned")
     .addPin('ClownKirby.jpg', 'Mirror Poyo', "Owned")
     .addPin('BottleKirby.jpg', 'Poyo in a Bottle', "Owned")
     .addPin('Clefairy.jpg', 'Clefairy Tamagotchi', "Owned")
+
+
 
 addCategory("Cute Animal Pins by Birduyen")
     .addPin('Mousie.jpg', 'Spring Mouse ☼ Flora Series', "Owned")
@@ -68,12 +78,12 @@ addCategory('Cute Animal Pins by Blushsprout', "https://blushsprout.carrd.co")
     .addPin('lionm.jpg', 'Lion Meringue Pie ☆ Rainbow Food Animals Series', "Owned")
     .addPin('raspbunny.jpg', 'Raspbunny Crepe ☆ Rainbow Food Animals Series', "Owned")
     .addPin('Fawnfetti.jpg', 'Fawnfetti ☆ Rainbow Food Animals Series', "Owned")
-    .addPin('Sloth.jpg', 'Cute Sloth ☆ Floranimals Series <br>(Seconds)', "Owned")
-    .addPin('night.jpg', 'Froggie ITA (Nighttime) Bag (Kickstarter) (Peach and Holo inserts)', "Owned")
+    .addPin('Sloth.jpg', 'Cute Sloth ☆ Floranimals Series', "Owned")
     .addPin('froggie pin bag.jpg', 'Froggie Bag Freebie Frog (Kickstarter)', "Owned")
     .addPin('witchfroggie.jpg', 'Witch Froggie <br>(October 2020 Pin Club)', "Owned")
     .addPin('daffodil.jpg', 'Daffodil Duckling Floranimals Series', "Owned")
     .addPin('kitten.jpg', 'Cherry Blossom Spring Kitten Mini Pin', "Owned")
+    .addPin('night.jpg', 'Froggie ITA (Nighttime) Bag (Kickstarter) (Peach and Holo inserts)', "Owned")
 
 addCategory('Cute Animal Pins by Goldfishkang', "https://goldfishkang.com")
     .addPin('LaundroCat.jpg', 'Laundrocats Pin: Dawn ☆', "Owned")
@@ -81,10 +91,16 @@ addCategory('Cute Animal Pins by Goldfishkang', "https://goldfishkang.com")
     .addPin('Adventurer.jpg', 'Little Adventurer ❀', "Owned")
     .addPin('PainterFox.jpg', 'Artsy Fox: Cloud Painter☆', "Owned")
 
-addCategory('Mamobot', "https://linktr.ee/mamobot")
-    .addPin('helioschibiusa.jpg', 'Chibiusa and Helios (Sailor Moon Fashion Scouts V3 Preorder)', "Owned")
-    .addPin('humanluna.jpg', 'Human Luna <br>(Sailor Moon Fashion Scouts V3 Preorder)', "Owned")
-    .addPin('smfamily.jpg', 'Luna, Artemis and Diana kitties <br>(Sailor Moon Fashion Scouts V3 Preorder PATREON EXCLUSIVE)', "Owned")
+
+addCategory('Mamobot Sailor Moon Pins', "https://linktr.ee/mamobot")
+    .addPin('helioschibiusa.jpg', 'Chibiusa and Helios', "Owned")
+    .addPin('humanluna.jpg', 'Human Luna', "Owned")
+    .addPin('smfamily.jpg', 'Luna, Artemis and Diana kitties', "Owned")
+    .addPin('SSailor.jpg', 'Super Sailor Moon', "Waiting")
+    .addPin('SSerenity.jpg', 'Serenity Sailor Moon', "Waiting")
+    .addPin('SCosmos.jpg', 'Cosmos Sailor Moon', "Waiting")
+
+
 
 addCategory("Polymernai", "https://www.polymernai.com")
     .addPin('VioletEvergarden.png', 'Violet x Gilbert Pin <br>(Nostalgia Couples KS)', "Owned")
@@ -92,11 +108,12 @@ addCategory("Polymernai", "https://www.polymernai.com")
     .addPin('Ladynoir.png', 'Ladybug x Cat Noir Pin <br>(Nostalgia Couples KS)', "Owned")
     .addPin('chibicatnoir.jpg', 'Chibi Cat Noir <br> by AkuoArt', "Owned")
 
+
 addInlineCategory()
     .addPin('Lilycli (UpbeatPals)', "https://lilycli.com", 'MyBuns.jpg', 'Wangxian Bunnies', "Owned")
     .addPin('Sharodactylart', "https://sharodactylart.com", 'BirthdayBun.jpg', 'Sprinkle Birthday Bunny', "Owned") 
     .addPin("Quirkory", "https://quirkory.carrd.co", 'raymond.jpg', 'Raymond Joy-Con (Animal Crossing)', "Owned")
-    
+
 
 addCategory('Weishi', "https://linktr.ee/weishi_art")
     .addPin('Weishi.jpg', 'Lily`s Hanami (September 2020 Pin Club)', "Owned")
