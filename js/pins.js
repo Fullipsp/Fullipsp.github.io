3bb2363efd1b0bd18591074ba75bb28e475c6538
Supertiger <saawalkhn@googlemail.com>
2022-12-12 21:03:57 +0000

    Fixed

--- js/pins.js
+++ js/pins.js
@@ -165,6 +165,7 @@ function addCategory(title, url) {
             if (lowercasePin === "owned") return 'done';
             if (lowercasePin === "shipped") return 'local_shipping';
             if (lowercasePin === "pre-ordered") return 'shopping_bag';
+            if (lowercasePin === "sold") return 'monetization_on';
         }
 
         const el = `
