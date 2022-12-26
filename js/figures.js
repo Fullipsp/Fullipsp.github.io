7451fb14deb975090c6e2083e599b783d7b7972e
Supertiger <saawalkhn@googlemail.com>
2022-12-26 15:15:05 +0000

    Added figures

---
+++ js/figures.js
@@ -0,0 +1,120 @@
+// cSpell:disable
+let categoryIndex = 0;
+
+
+addCategory("Placeholder", "http://msg.cheritz.com/")
+    .addFigure('gebby.jpg', 'Placeholder', "Owned")
+
+
+function closeFigure() {
+    const el = document.getElementById("preview");
+    el.firstChild.remove();
+    el.style.display = "none";
+
+}
+
+function showFigure(imageName) {
+    const el = document.getElementById("preview");
+    el.setAttribute("onClick", "closeFigure()");
+    el.setAttribute("oncontextmenu", "return false;");
+    el.style.display = "flex";
+    const img = new Image();
+    img.src = `images/figures/${imageName}`;
+    el.append(img);
+}
+
+
+function addCategory(title, url) {
+    categoryIndex++;
+    let currentIndex = categoryIndex;
+
+    const contentEl = document.querySelector(".content");
+
+    const categoryTitleEl = `<a class="category-title" href="${url}" target="_blank" rel="noopener noreferrer">${title}</a>`;
+    contentEl.innerHTML+=(categoryTitleEl);
+
+    const categoryEl = document.createElement("div");
+    categoryEl.id = `category-${currentIndex}`
+    categoryEl.classList.add("category");
+    contentEl.append(categoryEl);
+
+    this.addFigure = (figureImage, figureTitle, figureStatus) => {
+        const iconName = () => {
+            const lowercaseFigure = figureStatus.toLowerCase();
+            if (lowercaseFigure === "waiting") return 'schedule';
+            if (lowercaseFigure === "owned") return 'done';
+            if (lowercaseFigure === "shipped") return 'local_shipfigureg';
+            if (lowercaseFigure === "pre-ordered") return 'shopfigureg_bag';
+            if (lowercaseFigure === "sold") return 'monetization_on';
+        }
+
+        const el = `
+            <div class="figure-item">
+                <img oncontextmenu="return false;" onClick="showFigure('${figureImage}')" src="images/figures/${figureImage}" loading="lazy" alt="${figureTitle}" />
+                <div class="figure-title">${figureTitle}</div>
+                <div class="status ${figureStatus.toLowerCase()}"><span class="material-icons">${iconName()}</span>${figureStatus}</div>
+            </div>
+        `;
+        document.getElementById(categoryEl.id).innerHTML+= el;
+        return this;
+    }
+
+    return this;
+}
+function addInlineCategory() {
+    categoryIndex++;
+    let currentIndex = categoryIndex;
+
+    const contentEl = document.querySelector(".content");
+
+
+    const categoryEl = document.createElement("div");
+    categoryEl.id = `category-${currentIndex}`
+    categoryEl.classList.add("category", "inline");
+    contentEl.append(categoryEl);
+
+    this.addFigure = (title, url, figureImage, figureTitle, figureStatus) => {
+        const iconName = () => {
+            const lowercaseFigure = figureStatus.toLowerCase();
+            if (lowercaseFigure === "waiting") return 'schedule';
+            if (lowercaseFigure === "owned") return 'done';
+            if (lowercaseFigure === "shipped") return 'local_shipfigureg';
+            if (lowercaseFigure === "pre-ordered") return 'shopfigureg_bag';
+            if (lowercaseFigure === "sold") return 'monetization_on';
+        }
+
+        const el = `
+            <div class="figure-outer">
+                <a class="category-title" href="${url}" target="_blank" rel="noopener noreferrer">${title}</a>
+                <div class="figure-item">
+                    <img oncontextmenu="return false;" onClick="showFigure('${figureImage}')" src="images/figures/${figureImage}" loading="lazy" alt="${figureTitle}" />
+                    <div class="figure-title">${figureTitle}</div>
+                    <div class="status ${figureStatus.toLowerCase()}"><span class="material-icons">${iconName()}</span>${figureStatus}</div>
+              </div>
+           </div>
+        `;
+        document.getElementById(categoryEl.id).innerHTML+= el;
+        return this;
+    }
+
+    return this;
+}
+
+
+const categoryEl = document.querySelector(".figure-item");
+const contentEl = document.querySelector(".content")
+
+
+const moveTitle = () => {
+    const categoryLeft = categoryEl.getBoundingClientRect().left;
+    const contentLeft = contentEl.getBoundingClientRect().left;
+
+    document.querySelectorAll(".category-title").forEach(el => {
+        if (el.parentElement.classList.contains("figure-outer")) return;
+        el.style.transform = `translate(${categoryLeft - contentLeft}px, 0)`;
+    });
+}
+
+moveTitle();
+
+window.addEventListener("resize", moveTitle)
--- js/global.js
+++ js/global.js
@@ -6,6 +6,7 @@ const header = `
         <div class="links">
             <a class="link" href="./drawings.html"> <span class="material-icons">draw</span> My Drawings</a>
             <a class="link" href="./pins.html"> <span class="material-icons">collections</span> My Pin Collection</a>
+            <a class="link" href="./figures.html"> <span class="material-icons">settings_accessibility</span> My Figure Collection</a>
         </div>
     </header>
 `;
