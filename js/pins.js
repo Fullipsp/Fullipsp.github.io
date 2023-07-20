92c5158f55ac2e99b0512d3f3ddf4fbf826ffcc5
Fullipsp <69165390+Fullipsp@users.noreply.github.com>
2023-07-20 14:15:50 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -20,8 +20,8 @@ addCategory("For Sale - (Contact me on any of my socials, if interested!)")
 
 addCategory("MXTX Pins", "https://shorturl.at/iwxT3")
 
-    .addPin('xlwindow.webp', 'Xie Lian Window <br>by Peonypinsco', "Pre-Ordered")
-    .addPin('hcwindow.webp', 'Hua Cheng Window <br>by Peonypinsco', "Pre-Ordered")
+    .addPin('xlwindow.jpg', 'Xie Lian Window <br>by Peonypinsco', "Pre-Ordered")
+    .addPin('hcwindow.jpg', 'Hua Cheng Window <br>by Peonypinsco', "Pre-Ordered")
     .addPin('charityxl.webp', 'Xie Lian Charity Pin <br>by Peonypinsco', "Owned")
     .addPin('charityhc.webp', 'Hua Cheng Charity Pin <br>by Peonypinsco', "Owned")
     .addPin('honeyhc.webp', 'Devoted Bee-liever <br>by Honey Gokko', "Owned")
@@ -35,7 +35,7 @@ addCategory("MXTX Pins", "https://shorturl.at/iwxT3")
      .addPin('sitbfl.webp', 'Funny Beefleaf <br>by qqnzhe', "Owned")
     .addPin('lunechibfl.webp', 'Oriental Beefleaf <br>by MyLunechi', "Owned")
      .addPin('bbysqx.webp', 'Snow White Beefleaf <br>by unknown', "Owned")
-     .addPin('chibileaf.webp', 'Chibi Beefleaf', "Shipped")
+     .addPin('chibileaf.jpg', 'Chibi Beefleaf', "Shipped")
 
 .addPin('papoosh2.webp', 'Mermay Hualian <br>by Pap00sh', "Owned")   
 .addPin('papoosh.webp', 'Mermay Hualian <br>by Pap00sh', "Pre-Ordered")
@@ -51,15 +51,15 @@ addCategory("MXTX Pins", "https://shorturl.at/iwxT3")
     .addPin('wigglyhc.webp', 'Wiggly Fox HC', "Owned")
     .addPin('wigglyxl.webp', 'Wiggly Ferret XL', "Owned")
     .addPin('magicalhualian.webp', 'Hualian Being Cute', "Owned")
-    .addPin('bachhualian2.webp', 'Hualian <br>by Bachoochi', "Owned")
+    .addPin('bachhualian2.jpg', 'Hualian <br>by Bachoochi', "Owned")
    //
    //MDZS
-    .addPin('bachwangxian.webp', 'Lilypad Wangxian <br>by Bachoochi', "Shipped")
+    .addPin('bachwangxian.jpg', 'Lilypad Wangxian <br>by Bachoochi', "Shipped")
     .addPin('bluhdonkey.webp', 'Wangxian Donkey <br>by Bluhtack', "Owned")
     .addPin('bluhroof.webp', 'Wangxian on Roof <br>by Bluhtack', "Owned")
     .addPin('weiying.webp', 'Valentine Wei Ying <br>by Billacookie', "Owned")
     .addPin('MyBuns.webp', 'Wangxian Bunnies <br>by Lilycli', "Owned")
-    .addPin('woodenwangxian.webp', 'Wooden Wangxian Bunniess', "Shipped")
+    .addPin('woodenwangxian.jpg', 'Wooden Wangxian Bunniess', "Shipped")
 
 
 
