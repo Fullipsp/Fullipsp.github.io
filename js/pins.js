6283378bb1f91ffb01bd9bb71261f7c205d79e77
Fullipsp <69165390+Fullipsp@users.noreply.github.com>
2023-08-26 14:52:24 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -3,12 +3,12 @@ let categoryIndex = 0;
 
 addCategory("For Sale - (Contact me on any of my socials, if interested!)")
 
+    .addPin('lumine.webp', 'Lumine <br>by Sugar Cubed Studios', "For-Sale")
     .addPin('luminechibiprofile.webp', 'Chibi Lumine <br>by AkuoArt', "For-Sale")
     .addPin('childechibiprofile.webp', 'Chibi Childe Bust <br>by AkuoArt', "For-Sale")
     .addPin('xiaochibiprofile.webp', 'Chibi Xiao <br>by AkuoArt', "For-Sale")
     .addPin('albedochibiprofile.webp', 'Chibi Albedo <br>by AkuoArt', "For-Sale")
     .addPin('zhonglichibiprofile.webp', 'Chibi Zhongli <br>by AkuoArt', "For-Sale") 
-    .addPin('lumine.webp', 'Lumine <br>by Sugar Cubed Studios', "For-Sale")
     .addPin('bunnywitch.jpg', 'Snow Bunny Witch <br>by Mamobot', "For-Sale")
     .addPin('chilumi.webp', 'Chilumi <br>by Hollichoo', "For-Sale")
     .addPin('promise.webp', 'Snezhnayan Promise <br>by Hollichoo', "For-Sale")
