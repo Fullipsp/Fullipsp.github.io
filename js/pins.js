2f7fb3b2221440dd2341b692f747c1f2fe82e406
Fulli <69165390+Fullipsp@users.noreply.github.com>
2023-09-17 14:39:34 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -43,8 +43,11 @@ addCategory("Ordered (u Babci)")
 .addPin('sherb.webp', 'Dreaming Sherb <br>by haiyun', "Ordered")
 .addPin('punchy.webp', 'Rainy Punchy <br>by haiyun', "Ordered")
 .addPin('gachagorou.jpg', 'Chibi Gorou <br>by hideawaymelon', "Ordered")
-  
+
+  addGap();
 // =====================================================================================================================================================================================================================
+addCategory("Ordered (do mnie)")
+    
 .addPin('tarotbug.png', 'Ladybug Sun Tarot Card <br>LE60? by Paper*****ns', "Ordered")
 .addPin('tarotnoir.jpg', 'Cat Noir Moon Tarot Card <br>LE60 by Paper*****ns', "Ordered")
     
