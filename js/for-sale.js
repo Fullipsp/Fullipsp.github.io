1e63641cc7b8ce9e377d0ef6881f7ccc2fd350d4
Fulli <69165390+Fullipsp@users.noreply.github.com>
2023-11-26 17:17:36 +0000

    Update for-sale.js

--- js/for-sale.js
+++ js/for-sale.js
@@ -3,12 +3,12 @@ let categoryIndex = 0;
 
 addCategory("For Sale! - DM me if interested! (Discord: hualian.exe)")
 
-.addPin('inzelda.webp', 'BOTW Zelda <br>LE50 by Ins***Pins', "Owned")
+.addPin('inzelda.webp', 'BOTW Zelda <br>LE50 by Ins***Pins', "For-Sale")
 .addPin('Clefairy.webp', 'Clefairy Tamagotchi <br>by Arcade Healer', "For-Sale")
 .addPin('FS.jpg', 'Hualian Window <br>by SerenaDanmeiPins', "For-Sale")
-.addPin('fiveitto.webp', 'Itto <br>by Fiveonthe', "Owned")
-.addPin('fivethoma.webp', 'Thoma <br>by Fiveonthe', "Owned")
-.addPin('lunalbedo.webp', 'Kitty Albedo <br>by angryeggmerch', "Owned")
+.addPin('fiveitto.webp', 'Itto <br>by Fiveonthe', "For-Sale")
+.addPin('fivethoma.webp', 'Thoma <br>by Fiveonthe', "For-Sale")
+.addPin('lunalbedo.webp', 'Kitty Albedo <br>by angryeggmerch', "For-Sale")
 
 .addPin('chibichilde.webp', 'Chibi Childe <br>by AkuoArt', "For-Sale")
 .addPin('childechibiprofile.webp', 'Chibi Childe Bust <br>by AkuoArt', "For-Sale")
