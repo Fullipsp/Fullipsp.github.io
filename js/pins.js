b86ef37559a3784e3e453785e2dfea3982361ebf
Fulli <69165390+Fullipsp@users.noreply.github.com>
2024-08-12 14:18:03 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -23,16 +23,18 @@ addCategory("Ordered 🛒🛍")
 .addPin('xqstsg.webp', 'Gojo x Geto Set <br>by XQIU', "Ordered")
 .addPin('xqonepiece.webp', 'ASL One Piece Set <br>by XQIU', "Ordered")
 .addPin('xqobamitsu.webp', 'Obanai x Mitsuri Set <br>by XQIU', "Ordered")
-// Genshin
-.addPin('insomniacynari.webp', 'Cynari Diamond Pins <br>LE by Ins***Pins.', "Ordered")
+
 //YookiStudios
 .addPin('yookiloid.webp', 'Loid Mirror <br>by YookiStudios', "Ordered")
 .addPin('yookiyor.webp', 'Yor Mirror <br>by YookiStudios', "Ordered")
 .addPin('yookianya.webp', 'Anya Mirror <br>by YookiStudios', "Ordered")
-.addPin('yookimitsuri.webp', 'Kitsune Mitsuri <br>by YookiStudios', "Ordered")
-.addPin('yookiobanai.webp', 'Snake Obanai <br>by YookiStudios', "Ordered")
 .addPin('yookispohie.webp', 'HMC: Sophie Chibi <br>by YookiStudios', "Ordered")
 .addPin('yookihowl.webp', 'HMC: Howl Chibi <br>by YookiStudios', "Ordered")
+.addPin('yookimitsuri.webp', 'Kitsune Mitsuri <br>by YookiStudios', "Ordered")
+.addPin('yookiobanai.webp', 'Snake Obanai <br>by YookiStudios', "Ordered")
+// Genshin
+.addPin('insomniacynari.webp', 'Cynari Diamond Pins <br>LE by Ins***Pins.', "Ordered")
+
 
 .addPin('denjer.webp', 'Denji Diamond <br>by KassieArts', "Ordered")
 addGap();
