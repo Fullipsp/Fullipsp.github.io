dcff32a649d91b3fa4c953b44077a0d54460fdf5
Fulli <69165390+Fullipsp@users.noreply.github.com>
2024-08-25 15:13:13 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -56,20 +56,20 @@ addCategory("Foxipins MXTX Windows")
 .addPin('peiming.jpg', 'Pei Ming Window <br>LE65 by Foxipins', "Owned")
 .addPin('lingwen.jpg', 'Ling Wen Window <br>LE65 by Foxipins', "Owned")
 .addPin('shiwudu.jpg', 'Shi Wudu Window <br>LE65 by Foxipins', "Owned")
+
 .addPin('gongyixiao.webp', 'Gongyi Xiao Window <br>by Foxipins', "Ordered")
 .addPin('jiangcheng.webp', 'Jiang Cheng Window <br>by Foxipins', "Ordered")
 .addPin('yanli.webp', 'Jiang Yanli Window <br>by Foxipins', "Ordered")
 .addPin('wenqing.webp', 'Wen Qing Window <br>by Foxipins', "Ordered")
 .addPin('banyue.webp', 'Banyue Window <br>LE65 by Foxipins', "Sold")
 
-
-
-.addPin('binghe.jpg', 'Luo Binghe Window <br>by Foxipins', "Ordered")
 .addPin('hxwindow.webp', 'He Xuan Window <br>LE? by Foxipins', "Ordered")
 .addPin('sqxwindow.webp', 'Shi Qingxuan Window <br>LE? by Foxipins', "Ordered")
+.addPin('binghe.jpg', 'Luo Binghe Window <br>by Foxipins', "Ordered")
+.addPin('xichen.jpg', 'Lan Xichen Window <br>by Foxipins', "Ordered")
 .addPin('lwjpanel.jpg', 'Lan Wangji Window <br>by Foxipins', "Ordered")
 .addPin('foxiwwx.webp', 'Wei Wuxian Window <br>by Foxipins', "Ordered")
-.addPin('xichen.jpg', 'Lan Xichen Window <br>by Foxipins', "Ordered")
+ 
 .addPin('bingpup.webp', 'Bingpup Window <br>by Foxipins', "Ordered")
 .addPin('foxifairy.webp', 'Fairy Window <br>by Foxipins', "Ordered")
 
