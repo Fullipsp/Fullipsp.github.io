569c17cdbad12ac9b48a8806d6315bd649df5970
Fulli <69165390+Fullipsp@users.noreply.github.com>
2024-10-24 12:49:26 +0100

    Update pins.js

--- js/pins.js
+++ js/pins.js
@@ -11,8 +11,7 @@ addCategory("Ordered 🛒🛍")
 // PREORDERS
 .addPin('kmoribflf.webp', 'Kmori Beefleaf <br>LE? by Foxipins', "Ordered")
 .addPin('insomniadanheng.webp', 'Dan Heng Diamond Pins <br>LE by Ins***Pins.', "Ordered")
-.addPin('xxartl.webp', 'XXARTL Shiguang <br>LE? by riitsuki', "Ordered")
-.addPin('p4pshiguang.webp', 'P4P Tides Shiguang <br>LE? by flyingcanoepins', "Ordered")
+
 addGap();
 // =====================================================================================================================================================================================================================================================
 addCategory("Foxipins MXTX Windows")
@@ -209,6 +208,8 @@ addCategory("JJK Pins 🤍🖤")
 .addPin('beloved.webp', 'My Beloved <br> by WoupsShop', "Owned")
 // =========================================================================================================================================================================================================================
  addCategory("Link Click Pins 📸🔗🤍 ")
+.addPin('xxartl.webp', 'XXARTL Shiguang <br>LE? by riitsuki', "Ordered")
+.addPin('p4pshiguang.webp', 'P4P Tides Shiguang <br>LE? by flyingcanoepins', "Ordered")
 .addPin('goldxiaoshi.webp', 'Cheng Xiaoshi Panel - Gold <br>by ipinboys', "Owned")
 .addPin('goldguang.webp', 'Lu Guang Panel - Gold <br>x2 by ipinboys', "Owned")
 .addPin('cxschibi.webp', 'Cheng Xiaoshi Chibi <br>by flyingcanoepins', "Owned")
