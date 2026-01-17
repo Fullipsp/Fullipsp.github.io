8c62204b11d129dbb79f38d052813cd7ab04f67e
Fulli <69165390+Fullipsp@users.noreply.github.com>
2026-01-17 20:58:39 +0000

    Update drawings.js

--- js/drawings.js
+++ js/drawings.js
@@ -1,10 +1,12 @@
 // cSpell:disable
 const categoryEl = document.querySelector(".category");
 
+addDrawing('freakynoir.webp', 'Cl4w N0ir Fanart');
+addDrawing('emofurry.webp', 'Fishie Fursona hehe');
+addDrawing('bobaqueens.webp', 'Boba Queens BDay');
 addDrawing('to postie.webp', 'MXTX Best Boy Trio <br>MDZS | TGCF | SVSSS');
 addDrawing('hualianp.webp', 'Hualian Portrait <br>Tian Guan Ci Fu');
 addDrawing('AxL.webp', 'Arwin & Latte Portrait <br>Miss Not So Sidekick');
-
 addDrawing('uzui.webp', 'Uzui Birthday Party (for my mom) <br>Demon Slayer');
 addDrawing('gorouarcher.webp', 'Gorou Bow Practice <br>Genshin Impact');
 addDrawing('catxiao.webp', 'Catboy Xiao <br>Genshin Impact');
@@ -15,25 +17,19 @@ addDrawing('zagreus.webp', 'Zagreus <br>Hades');
 addDrawing('kazuhabday.webp', 'Kazuhas Birthday <br>Genshin Impact');
 addDrawing('tomo.webp', 'Kazuhas Friend <br>Genshin Impact');
 addDrawing('winterxiao.webp', 'Winter Xiao - Otome AU <br>Genshin Impact');
-addDrawing('genshincomic.webp', 'My Old Genshin Team <br>Genshin Impact');
-
 addDrawing('celestial.webp', 'Celestial Dragon OC');
 addDrawing('hollydragon.webp', 'Holly Christmas Dragon');
-
-
 addDrawing('rayphone.webp', 'Ray on the Phone <br>Mystic Messenger');
-
 addDrawing('belpher.webp', 'Belphegor Underwater <br>Obey Me');
 addDrawing('leviathan.webp', 'Leviathan <br>Obey Me');
-
 addDrawing('gesaeran.webp', 'Saeran Portrait <br>Mystic Messenger');
 addDrawing('kofi.webp', 'Saeran Drinking Ko-fi :3 <br>Mystic Messenger');
 addDrawing('sxm.webp', 'Saeran x Me Portrait <br>Mystic Messenger');
 addDrawing('mxs.webp', 'Saeran x MC <br>Mystic Messenger');
-
-
 addDrawing('chongyunsketch.webp', 'Drummer Chongyun WIP <br>Genshin Impact');
-
+addDrawing('astarionchibi.webp', 'Astarion x Arsene Chibi');
+addDrawing('myocsketch.webp', 'Arsene Sketch BG3');
+addDrawing('genshincomic.webp', 'My Old Genshin Team <br>Genshin Impact');
 addDrawing('6challenge.webp', '6 Characters Challenge');
 
 function closeDrawing() {
@@ -61,6 +57,7 @@ function addDrawing (image, title) {
     `;
     categoryEl ? (categoryEl.innerHTML+= el) : null;
 }
+
 
 
 
