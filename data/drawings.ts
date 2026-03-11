ad7f0e8905b94dcd4cd62f787dcc1c295b691d2f
Fullipsp <laura.mojra@gmail.com>
2026-03-11 01:13:14 +0000

    u

--- data/drawings.ts
+++ data/drawings.ts
@@ -12,11 +12,11 @@ drawing("winterxiao.webp", "Winter Xiao - Otome AU", "Genshin Impact 2021");
 drawing("tomo.webp", "Kazuhas Friend", "Genshin Impact 2021");
 drawing("kazuhabday.webp", "Kazuhas Birthday", "Genshin Impact");
 drawing("childe.webp", "Childe - Tartaglia", "Independence Day 2021");
-drawing("catnoir.webp", "Cat Noir", "Miraculous Ladybug");
-drawing("celestial.webp", "Celestial Dragon OC ", "Art Trade");
-drawing("belpher.webp", "Belphegor Underwater", "Obey Me");
-drawing("chongyunsketch.webp", "Drummer Chongyun WIP", "Genshin Impact");
-drawing("rayphone.webp", "Ray on the Phone", "Mystic Messenger");
+drawing("catnoir.webp", "Cat Noir", "Miraculous Ladybug 2021");
+drawing("celestial.webp", "Celestial Dragon OC ", "Art Trade 2022");
+drawing("belpher.webp", "Belphegor Underwater", "Obey Me 2021");
+drawing("chongyunsketch.webp", "Drummer Chongyun WIP", "Genshin Impact 2021");
+drawing("rayphone.webp", "Ray on the Phone", "Mystic Messenger 2021");
 
 drawing("bobaqueens.webp", "Boba Queens BDay", "2025");
 drawing("to postie.webp", "MXTX Best Boy Trio", "MXTX 2024");
@@ -24,16 +24,16 @@ drawing("uzui.webp", "Uzui Birthday Party", "Demon Slayer 2023");
 drawing("hollydragon.webp", "Holly Christmas Dragon", "Bday Gift 2022");
 drawing("kofi.webp", "Saeran Drinking Ko-fi :3", "Mystic Messenger 2021");
 
-drawing("thanatos.webp", "Thanatos", "Hades");
-drawing("zagreus.webp", "Zagreus", "Hades");
-drawing("mxs.webp", "Saeran x MC", "Mystic Messenger");
-drawing("sxm.webp", "Saeran x MC Portrait", "Mystic Messenger");
-drawing("gesaeran.webp", "Saeran Portrait", "Mystic Messenger");
-drawing("astarionchibi.webp", "Astarion x Arsene Chibi", "BaldursGate3");
-drawing("myocsketch.webp", "Arsene Sketch BG3", "BaldursGate3");
-drawing("leviathan.webp", "Leviathan", "Obey Me");
-drawing("genshincomic.webp", "My Old Genshin Team", "Genshin Impact");
-drawing("6challenge.webp", "6 Characters Challenge", "Multifandom");
+drawing("thanatos.webp", "Thanatos", "Hades 2021");
+drawing("zagreus.webp", "Zagreus", "Hades 2021");
+drawing("mxs.webp", "Saeran x MC", "Mystic Messenger 2021");
+drawing("sxm.webp", "Saeran x MC Portrait", "Mystic Messenger 2021");
+drawing("gesaeran.webp", "Saeran Portrait", "Mystic Messenger 2021");
+drawing("astarionchibi.webp", "Astarion x Arsene Chibi", "BaldursGate3 2024");
+drawing("myocsketch.webp", "Arsene Sketch BG3", "BaldursGate3 2024");
+drawing("leviathan.webp", "Leviathan", "Obey Me 2021");
+drawing("genshincomic.webp", "My Old Genshin Team", "Genshin Impact 2022");
+drawing("6challenge.webp", "6 Characters Challenge", "Multifandom 2021");
 
 export interface Drawing {
   src: string;
