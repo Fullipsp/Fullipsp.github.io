9dfb86a6f539ed23d6948d78c25873aec18c483a
Fullipsp <laura.mojra@gmail.com>
2026-03-13 22:18:58 +0000

    w

--- data/pin-displays.ts
+++ data/pin-displays.ts
@@ -6,30 +6,32 @@ display("/pins/orangepinz.webp", "WIP", "UPDATE");
 display("/pins/yellowpinz.webp", "WIP", "UPDATE");
 display("/pins/greenpinz.webp", "WIP", "UPDATE");
 display("/pins/bluepinz.webp", "WIP", "UPDATE");
-display("/pins/purplepinz.webp", "WIP", "UPDATE");
 
-
-
-display("/pins/zuchibiboard.webp", "WIP", "UPDATE");
-display("/pins/jjkboard.webp", "WIP", "UPDATE");
+display("/pins/astarionboard.webp", "WIP", "UPDATE");
 display("/pins/bingqiuboard.webp", "WIP", "UPDATE");
 display("/pins/mdzsboard.webp", "WIP", "UPDATE");
-display("/pins/astarionboard.webp", "WIP", "UPDATE");
-display("/pins/neuviboard.webp", "WIP", "UPDATE");
+display("/pins/jjkboard.webp", "WIP", "UPDATE");
+display("/pins/zuchibiboard.webp", "WIP", "UPDATE");
+
+display("/pins/genshindiamondboard.webp", "WIP", "UPDATE");
 display("/pins/helluvaboard.webp", "WIP", "UPDATE");
 display("/pins/shadyclawboard.webp", "WIP", "UPDATE");
-display("/pins/genshindiamondboard.webp", "WIP", "UPDATE");
-display("/pins/botwboard.webp", "WIP", "UPDATE");
+display("/pins/neuviboard.webp", "WIP", "UPDATE");
 display("/pins/ivantillboard.webp", "WIP", "UPDATE");
+
+display("/pins/botwboard.webp", "WIP", "UPDATE");
 display("/pins/ranwanboard.webp", "WIP", "UPDATE");
 display("/pins/linkclickboard.webp", "WIP", "UPDATE");
-display("/pins/pastelboard.webp", "WIP", "UPDATE");
-display("/pins/hualianboard.webp", "WIP", "UPDATE");
-display("/pins/foxiboard1.webp", "WIP", "UPDATE");
-display("/pins/foxiboard2.webp", "WIP", "UPDATE");
 display("/pins/tarotboard.webp", "WIP", "UPDATE");
+display("/pins/purplepinz.webp", "WIP", "UPDATE");
+
+display("/pins/hualianboard.webp", "WIP", "UPDATE");
 display("/pins/orangeloveboard.webp", "WIP", "UPDATE");
 display("/pins/diamondglassboard.webp", "WIP", "UPDATE");
+display("/pins/foxiboard1.webp", "WIP", "UPDATE");
+display("/pins/foxiboard2.webp", "WIP", "UPDATE");
+
+display("/pins/pastelboard.webp", "WIP", "UPDATE");
 
 
 
