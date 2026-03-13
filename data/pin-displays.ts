3c39fc422969986e5f1ebb27aaab1b4badd3f996
Fullipsp <laura.mojra@gmail.com>
2026-03-13 21:13:15 +0000

    update pin displays 1

--- data/pin-displays.ts
+++ data/pin-displays.ts
@@ -1,11 +1,28 @@
 import { createStore } from "solid-js/store";
 const [_displays, setDisplays] = createStore<Display[]>([]);
 
-display("/pins/Lily.webp", "WORK IN PROGRESS", "WILL UPDATE SOON");
-display("/pins/Lily.webp", "WORK IN PROGRESS", "WILL UPDATE SOON");
-display("/pins/Lily.webp", "WORK IN PROGRESS", "WILL UPDATE SOON");
-display("/pins/Lily.webp", "WORK IN PROGRESS", "WILL UPDATE SOON");
-display("/pins/Lily.webp", "WORK IN PROGRESS", "WILL UPDATE SOON");
+display("/pins/zuchibiboard.webp", "WIP", "UPDATE");
+display("/pins/jjkboard.webp", "WIP", "UPDATE");
+display("/pins/bingqiuboard.webp", "WIP", "UPDATE");
+display("/pins/mdzsboard.webp", "WIP", "UPDATE");
+display("/pins/astarionboard.webp", "WIP", "UPDATE");
+display("/pins/neuviboard.webp", "WIP", "UPDATE");
+display("/pins/helluvaboard.webp", "WIP", "UPDATE");
+display("/pins/shadyclawboard.webp", "WIP", "UPDATE");
+display("/pins/genshindiamondboard.webp", "WIP", "UPDATE");
+display("/pins/botwboard.webp", "WIP", "UPDATE");
+display("/pins/ivantillboard.webp", "WIP", "UPDATE");
+display("/pins/ranwanboard.webp", "WIP", "UPDATE");
+display("/pins/linkclickboard.webp", "WIP", "UPDATE");
+display("/pins/pastelboard.webp", "WIP", "UPDATE");
+display("/pins/hualianboard.webp", "WIP", "UPDATE");
+display("/pins/foxiboard1.webp", "WIP", "UPDATE");
+display("/pins/foxiboard2.webp", "WIP", "UPDATE");
+display("/pins/tarotboard.webp", "WIP", "UPDATE");
+display("/pins/orangeloveboard.webp", "WIP", "UPDATE");
+display("/pins/diamondglassboard.webp", "WIP", "UPDATE");
+
+
 
 export interface Display {
   src: string;
