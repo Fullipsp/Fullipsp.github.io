11d6828552af270eb1d28b7c0314a8a6467e36b0
Fullipsp <laura.mojra@gmail.com>
2026-03-30 22:33:22 +0100

    updated characters

--- data/fave-characters.ts
+++ data/fave-characters.ts
@@ -1,10 +1,20 @@
 import { createStore } from "solid-js/store";
 const [_displays, setDisplays] = createStore<Display[]>([]);
 
+display("/fave-characters/moran.webp", "Mo Weiyu", "Erha: Husky & Shizun");
+display("/fave-characters/cwn.webp", "Chu Wanning", "Erha: Husky & Shizun");
+display("/fave-characters/binghe.webp", "Luo Binghe", "Scum Villain System");
+display("/fave-characters/sqq.webp", "Shen Qingqiu", "Scum Villain System");
+display("/fave-characters/xielian.webp", "Xie Lian", "Heaven Officials Blessin'");
+display("/fave-characters/huacheng.webp", "Hua Cheng", "Heaven Officials Blessin'");
+display("/fave-characters/saeran.jpg", "Cat Noir", "Mir4culous Ladybug");
 
-
-
-display("/fave-characters/2minfizz.webp", "test", "test");
+display("/fave-characters/catnoir.jpg", "Cat Noir", "Mir4culous Ladybug");
+display("/fave-characters/xavier.jpg", "Sylus", "Love & Deepspace");
+display("/fave-characters/astarion.jpg", "Astarion", "Baldur's Gate 3");
+display("/fave-characters/sylus.jpg", "Sylus", "Love & Deepspace");
+display("/fave-characters/rafayel.jpg", "Rafayel", "Love & Deepspace");
+display("/fave-characters/xavier.jpg", "Xavier", "Love & Deepspace");
 
 
 
