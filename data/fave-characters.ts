4275848dce988d44b810b83e0034f4a67c3e4191
Fullipsp <laura.mojra@gmail.com>
2026-03-30 23:44:56 +0100

    update

--- data/fave-characters.ts
+++ data/fave-characters.ts
@@ -3,22 +3,34 @@ const [_displays, setDisplays] = createStore<Display[]>([]);
 
 display("/fave-characters/moran.webp", "Mo Weiyu", "Erha: Husky & Shizun");
 display("/fave-characters/cwn.webp", "Chu Wanning", "Erha: Husky & Shizun");
-display("/fave-characters/binghe.webp", "Luo Binghe", "Scum Villain System");
-display("/fave-characters/sqq.webp", "Shen Qingqiu", "Scum Villain System");
 display("/fave-characters/xielian.webp", "Xie Lian", "Heaven Officials Blessin'");
 display("/fave-characters/huacheng.webp", "Hua Cheng", "Heaven Officials Blessin'");
-display("/fave-characters/saeran.jpg", "Cat Noir", "Mir4culous Ladybug");
+display("/fave-characters/binghe.webp", "Luo Binghe", "Scum Villain System");
+display("/fave-characters/sqq.webp", "Shen Qingqiu", "Scum Villain System");
+display("/fave-characters/luguang.jpg", "Lu Guang", "Link Click");
+display("/fave-characters/chengxiaoshi.jpg", "Cheng Xiaoshi", "Link Click");
+
+display("/fave-characters/neuvilette.jpg", "Neuvilette", "Genshin Impact");
+display("/fave-characters/phainon.jpg", "Phainon / Khaslana", "Honkai Star Rail");
+display("/fave-characters/aventurine.jpg", "Aventurine", "Honkai Star Rail");
+display("/fave-characters/danheng.jpg", "Dan Heng", "Honkai Star Rail");
+display("/fave-characters/saeran.jpg", "Saeran Choi", "Mystic Messenger");
+display("/fave-characters/belphie.jpg", "Belphegor", "Obey Me!");
+display("/fave-characters/sasaki.jpg", "Sasaki", "Sasaki to Miyano");
+display("/fave-characters/saaki.jpg", "Sasaki", "Sasaki to Miyano");
 
 display("/fave-characters/catnoir.jpg", "Cat Noir", "Mir4culous Ladybug");
-display("/fave-characters/xavier.jpg", "Sylus", "Love & Deepspace");
 display("/fave-characters/astarion.jpg", "Astarion", "Baldur's Gate 3");
 display("/fave-characters/sylus.jpg", "Sylus", "Love & Deepspace");
 display("/fave-characters/rafayel.jpg", "Rafayel", "Love & Deepspace");
 display("/fave-characters/xavier.jpg", "Xavier", "Love & Deepspace");
 
+display("/fave-characters/lifeweaver.jpg", "Lifeweaver", "Overwatch");
+display("/fave-characters/wuyang.jpg", "Wuyang", "Overwatch");
+display("/fave-characters/mizuki.jpg", "Mizuki", "Overwatch");
 
-
-
+display("/fave-characters/stolas.jpg", "Stolas", "Helluva Boss");
+display("/fave-characters/lucifer.jpg", "Lucifer", "Hazbin Hotel");
 
 
 
