07478da6a38387ded408e7518e71bb76f8cd9a81
Fullipsp <laura.mojra@gmail.com>
2026-04-03 01:18:55 +0100

    added many cahracters

--- data/fave-characters.ts
+++ data/fave-characters.ts
@@ -7,31 +7,50 @@ display("/fave-characters/xielian.webp", "Xie Lian", "Heaven Officials Blessin'"
 display("/fave-characters/huacheng.webp", "Hua Cheng", "Heaven Officials Blessin'");
 display("/fave-characters/binghe.webp", "Luo Binghe", "Scum Villain System");
 display("/fave-characters/sqq.webp", "Shen Qingqiu", "Scum Villain System");
-display("/fave-characters/luguang.jpg", "Lu Guang", "Link Click");
-display("/fave-characters/chengxiaoshi.jpg", "Cheng Xiaoshi", "Link Click");
+display("/fave-characters/tlj.jpg", "Tianlang-Jun", "Scum Villain System");
+display("/fave-characters/muqing.jpg", "Mu Qing", "Heaven Officials Blessin'");
 
+display("/fave-characters/weiwuxian.jpg", "Wei Wuxian", "Mo Dao Zu Shi");
 display("/fave-characters/neuvilette.jpg", "Neuvilette", "Genshin Impact");
 display("/fave-characters/phainon.jpg", "Phainon / Khaslana", "Honkai Star Rail");
 display("/fave-characters/moze.jpg", "Moze", "Honkai Star Rail");
-display("/fave-characters/aventurine.jpg", "Aventurine", "Honkai Star Rail");
 display("/fave-characters/danheng.jpg", "Dan Heng", "Honkai Star Rail");
-display("/fave-characters/saeran.jpg", "Saeran Choi", "Mystic Messenger");
-display("/fave-characters/belphie.jpg", "Belphegor", "Obey Me!");
-display("/fave-characters/sasaki.jpg", "Sasaki", "Sasaki to Miyano");
+display("/fave-characters/aventurine.jpg", "Aventurine", "Honkai Star Rail");
+display("/fave-characters/kaveh.jpg", "Kaveh", "Genshin Impact");
+display("/fave-characters/alhaitham.jpg", "Alhaitham", "Genshin Impact");
 
+display("/fave-characters/luguang.jpg", "Lu Guang", "Link Click");
+display("/fave-characters/chengxiaoshi.jpg", "Cheng Xiaoshi", "Link Click");
+display("/fave-characters/saeran.jpg", "Saeran Choi", "Mystic Messenger");
+display("/fave-characters/tamonk.jpg", "Tamon Fukuhara", "Tamon's B Side");
+display("/fave-characters/dazai.jpg", "Dazai Osamu", "Bungou Stray Dogs");
+display("/fave-characters/chuuya.jpg", "Chuuya Nakahara", "Bungou Stray Dogs");
+display("/fave-characters/tetchou.jpg", "Tetcho Suehiro", "Bungou Stray Dogs");
+display("/fave-characters/choso.jpg", "Choso", "Jujjutsu Kaisen");
+
+display("/fave-characters/sasaki.jpg", "Sasaki", "Sasaki to Miyano");
+display("/fave-characters/simon.jpg", "Simeon", "Obey Me!");
+display("/fave-characters/mammon.jpg", "Mammon", "Obey Me!");
+display("/fave-characters/belphie.jpg", "Belphegor", "Obey Me!");
+display("/fave-characters/thanatos.jpg", "Thanatos", "Hades I");
+display("/fave-characters/morpheus.jpg", "Morpheus", "Hades II");
+display("/fave-characters/stolas.jpg", "Stolas", "Helluva Boss");
+display("/fave-characters/lucifer.jpg", "Lucifer", "Hazbin Hotel");
 
 display("/fave-characters/catnoir.jpg", "Cat Noir", "Mir4culous Ladybug");
-display("/fave-characters/astarion.jpg", "Astarion", "Baldur's Gate 3");
+display("/fave-characters/lifeweaver.jpg", "Lifeweaver (Niran)", "Overwatch");
+display("/fave-characters/wuyang.jpg", "Wuyang Ye", "Overwatch");
+display("/fave-characters/mizuki.jpg", "Mizuki Kawano", "Overwatch");
+display("/fave-characters/astarion.jpg", "Astarion Ancunín", "Baldur's Gate 3");
 display("/fave-characters/sylus.jpg", "Sylus", "Love & Deepspace");
 display("/fave-characters/rafayel.jpg", "Rafayel", "Love & Deepspace");
 display("/fave-characters/xavier.jpg", "Xavier", "Love & Deepspace");
 
-display("/fave-characters/lifeweaver.jpg", "Lifeweaver", "Overwatch");
-display("/fave-characters/wuyang.jpg", "Wuyang", "Overwatch");
-display("/fave-characters/mizuki.jpg", "Mizuki", "Overwatch");
 
-display("/fave-characters/stolas.jpg", "Stolas", "Helluva Boss");
-display("/fave-characters/lucifer.jpg", "Lucifer", "Hazbin Hotel");
+
+
+
+
 
 
 
